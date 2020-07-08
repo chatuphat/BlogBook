da8bd19aca5eaa0db9af84b03d6854e2ce51265c
s60160275 <60160275@go.buu.ac.th>
2020-07-08 14:06:33 +0700

    Create form

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render("blogs/index",{data:"Hello mongoose"});
+  res.render("blogs/index",{data:"ข้อมูลบทความ"});
+});
+
+router.get('/add', function(req, res, next) {
+  res.render("blogs/addForm",{data:"เพิ่มบทความ"});
 });
 
 module.exports = router;
